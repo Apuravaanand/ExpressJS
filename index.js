56ebac644d2451944144b7f32de258285beb53da
@purava__ <115463013+Apuravaanand@users.noreply.github.com>
2026-02-03 18:26:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ app.use((req, res) => {
     res.status(404).send("404: Page not found i.e This page is not exist");
 });
 
-
 //===================Pathparameter : send a variable to server and server make a response with name=======================
 app.get("/username",(req, res) => {
     console.log(req.params);
@@ -44,10 +43,4 @@ app.get("/username/id",(req, res) => {
     let htmlstr = `<h1>Welcome Mister @${username}.</h1><h3>Your current id is ${id}</h3> `;
     res.send(htmlstr);
 });
-
-
-
-
-
-
 
